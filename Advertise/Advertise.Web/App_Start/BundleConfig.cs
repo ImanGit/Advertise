78cd4b284b8261a07676ce60501f311ad59e7b3e
ImanGit <iman4biz@gmail.com>
2016-07-09 21:37:13 +0430

    commit new files

---
+++ Advertise/Advertise.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace Advertise.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterByndle(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/bootstrap").Include("~/Content/bootstrap.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
