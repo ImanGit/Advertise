23aca3ceac7fc3d3c0507fd58776be45fa21618a
arezooAFM <genius.alc.2010@gmail.com>
2016-10-07 18:12:56 +0330

    ایجاد سرویسهای CompanySocial

---
+++ Advertise/Advertise.ViewModel/Models/Companies/CompanySocial/CompanySocialDeleteViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Advertise.ViewModel.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertise.ViewModel.Models.Companies.CompanySocial
+{
+  public   class CompanySocialDeleteViewModel : BaseViewModel
+
+    {
+
+        public Guid  Id { get; set; }
+        [DisplayName("آدرس تویتر")]
+        // [Required(ErrorMessage = "لطفا متن بررسی را وارد کنید")]
+        //[StringLength(50, ErrorMessage = "کد شناسه باید کمتر از ۵۰ کاراکتر باشد")]
+        public string TwitterLink { get; set; }
+
+        [DisplayName("آدرس فیس بوک")]
+        public string FacebookLink { get; set; }
+
+        [DisplayName("آدرس گوگل پلاس")]
+        public string GooglePlusLink { get; set; }
+
+        [DisplayName("آدرس یوتیوب")]
+        public string YoutubeLink { get; set; }
+
+    }
+}
